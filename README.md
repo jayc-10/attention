4ddc2d4f7cd49687788402630273d29ac5a41bed
Jay Chan <jayc10@uci.edu>
2024-12-30 00:04:07 -0800

    setup colors and centered content

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 <img src="public/cover.png">
 
-## your attention please ! (or not, but thanks for visiting 🤍)
+## 1&nbsp;&nbsp;: your attention please ! (or not, but thanks for visiting 🤍)
 in the seventh grade, i read two pages in half an hour. yes, two pages. i found myself re-reading the same lines over and over to where my head hurt and i struggled to reach the reading goals that were once easy for me. years went by and i never knew that this was a common indication of adhd.
 
 despite what could end up being huge workloads, i always found myself procrastinating work and even if i had started early i would still end up finishing my work late. "why didn't i start earlier?" i would tell myself. but at the end of the day, i knew that if i had, it wouldn't have made a difference.
@@ -12,3 +12,6 @@ growing up, i've learned of a few techniques that i want to share with others wh
 thank you for taking the time to read the inspiration behind this project!
 
 as 2024 comes to an end, i want to express my gratitude to the friends i've met this year for motivating me to pursue my passions: thank you josh, leo, nathan and danny.
+
+## 2&nbsp;&nbsp;: installation
+app currently still in development
--- app.py
+++ app.py
@@ -1,12 +1,65 @@
 import tkinter as tk
 from tkinter import messagebox
 
+
+# logic
+def countdown(count):
+    minutes, seconds = divmod(count, 60)
+    timer_label.config(text=f"{minutes:02}:{seconds:02}")
+    if count > 0:
+        window.after(1000, countdown, count - 1)
+    else:
+        messagebox.showinfo("attention", "good work! time to take a break!")
+
+
+def work():
+    countdown(1500)
+
+
+def short():
+    countdown(300)
+
+
+def long():
+    countdown(900)
+
+
+# window display
 window = tk.Tk()
-window.title("Attention")
-timer_label = tk.Label(window, text="00:00", font=("Inter", 28))
+window.title("attention")
+window.geometry("200x150")
+window.configure(bg="#B5C6E0")
+
+# frame (center info)
+frame = tk.Frame(window, bg="#B5C6E0")
+frame.pack(pady=20)
+
+# timer display
+timer_label = tk.Label(
+    window,
+    text="25:00",
+    font=(
+        "Inter",
+        28),
+    bg="#B5C6E0",
+    fg="#2A3B47")
 timer_label.pack()
 
-start_button = tk.Button(window, text="Start")
-start_button.pack()
+# timer button
+start_button = tk.Button(
+    window,
+    text="start",
+    font=("Inter"),
+    command=work,
+    bg="#B5C6E0",
+    fg="#2A3B47",
+    borderwidth=0)
+start_button.pack(pady=5)
 
-window.mainloop()
+
+def main():
+    window.mainloop()
+
+
+if __name__ == '__main__':
+    main()
